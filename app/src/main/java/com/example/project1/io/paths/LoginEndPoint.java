5906ec90b5f138cc1ea0f6fcf1b88d44804cb162
Giorgos Petropoulos <info@codeondesk.com>
2021-11-30 23:49:36 +0200

    commit2

--- app/src/main/java/com/example/project1/io/paths/LoginEndPoint.java
+++ app/src/main/java/com/example/project1/io/paths/LoginEndPoint.java
@@ -1,16 +1,10 @@
 package com.example.project1.io.paths;
-
 import com.example.project1.io.Login;
 import com.example.project1.io.User;
-
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
+
 
 public interface LoginEndPoint {
 
